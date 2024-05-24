2859863c6759aabb4cc9d062ae3d6a1ddcdb616e
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-05-24 17:28:03 -0300

    0.0.2

--- src/main/java/com/projetoPOO/restaurante/model/pedido/PedidoRepository.java
+++ src/main/java/com/projetoPOO/restaurante/model/pedido/PedidoRepository.java
@@ -3,7 +3,10 @@ package com.projetoPOO.restaurante.model.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.projetoPOO.restaurante.model.usuario.Usuario;
+
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
     //  local onde devo colocar as funções CRUD mais especificas
+   
 }
--- src/main/java/com/projetoPOO/restaurante/model/pedido/PedidoService.java
+++ src/main/java/com/projetoPOO/restaurante/model/pedido/PedidoService.java
@@ -13,6 +13,16 @@ public class PedidoService {
     private PedidoRepository pedidoRepository;
     //  local onde devo colocar todas as funções que serão chamadas no indexController
 
+    public Pedido alterarStatus(int id, boolean status) {
+        Pedido pedido = pedidoRepository.findAllById(id);
+        
+    }
+    
+    public Pedido buscarPedidoPorID(Pedido pedido) {
+        int id = pedido.getId();
+        return pedidoRepository.findById(id);
+    }
+    
     public Pedido inserirPedido(Pedido pedido) {
         return pedidoRepository.save(pedido);
     }
