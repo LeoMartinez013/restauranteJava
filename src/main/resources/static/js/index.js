a07401feb3afcd04d8eb7fa0b6768bf2cb3f50a0
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-05-28 20:35:56 -0300

    mini commit

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,61 @@
 /*
 verifSenha()
+
+var nome = $("#nome").val();
+var senha = $("#senha").val();
+var senhaRepetida = $("#senhaRepetida").val();
+
+var usuario = {
+    nome: nome,
+    senha: senha,
+    senhaRepetida: senhaRepetida
+};
+
+$.ajax({
+    type: "POST",
+    url: "/cadastro",
+    data: JSON.stringify(usuario),
+    contentType: "application/json",
+    success: function(data) {
+        console.log(data);
+        alert("Usuario Cadastrado com Sucesso!"
+        +"\nID: " + data.id + "\nNome: " + data.nome);
+    },
+    error: function(err) {
+        console.log(err);
+    }
+});
 */
 window.onload(() => {
-    
+    carregarCardapio()
+    //  listarPedidosProntos()
 })
+async function adicionarPedido() {
+    
+}
+async function adicionarItem() {
+    
+}
+async function carregarCardapio() {
+    $.ajax({
+        url: '/itens-cardapio',
+        type: 'GET',
+        success: function(data) {
+            data.content.forEach(function(itemCardapio) {
+                var button = $('<button/>', {
+                    text: itemCardapio.nome,
+                    id: 'itemCardapio' + itemCardapio.id,
+                    class: 'itemCardapio'
+                });
+                $('#cardapio').append(button);
+            });
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
+async function listarPedidosProntos() {
+
+}
