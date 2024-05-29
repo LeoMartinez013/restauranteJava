28c941bd150dfe311d8fe9ad4b251978ff7bd59b
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-05-28 22:16:21 -0300

    init index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,36 @@
-/*
-verifSenha()
-*/
-window.onload(() => {
+const containerCardapio = document.getElementById('cardapio')
+var itensPedido = []
+
+window.onload = function() {
+    //listarCardapio()
     
-})
+}
+async function addItemPedido(id) {
+    var checkbox = document.getElementById('checkItemCardapio' + 1)
+    if(checkbox.checked) {
+        checkbox.checked = false
+    } else {
+        checkbox.checked = true
+    }
+    console.log(checkbox.checked)
+}
+async function listarCardapio() {
+    $.ajax({
+        url: '/itens-cardapio',
+        type: 'GET',
+        success: function(data) {
+            
+            data.content.forEach(function(itemCardapio) {
+                var button = $('<button/>', {
+                    text: itemCardapio.nome,
+                    id: 'itemCardapio' + itemCardapio.id,
+                    class: 'itemCardapio'
+                });
+                $('#cardapio').append(button);
+            });
+        },
+        error: function(error) {
+            console.error(error);
+        }
+    });
+}
