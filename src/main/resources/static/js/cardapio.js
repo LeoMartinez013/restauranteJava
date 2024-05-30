0b77a9d16e4a7d802b685e71695fac20972692d7
LeoMartinez013 <105740782+LeoMartinez013@users.noreply.github.com>
2024-05-30 14:50:01 -0300

    Ajuste layout

---
+++ src/main/resources/static/js/cardapio.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    //listarCardapio()
+    
+}
+async function ListarCardapio() {
+    $.ajax({
+        url: '/itens-cardapio',
+        type: 'GET',
+        success: function(data) {
+            
+            data.content.forEach(function(itemCardapio) {
+                var button = $('<button/>', {
+                    text: itemCardapio.nome,
+                    id: 'itemCardapio' + itemCardapio.id,
+                    class: 'itemCardapio'
+                });
+                $('#cardapio').append(button);
+            });
+        },
+        error: function(error) {
+            console.error(error);
+        }
+    });
+}
